c74ec8570804886aac928ea5cc6c28f1d1c5f1af
Maged Fathy Ghazouly <maguedfathey@gmail.com>
2017-08-12 00:14:55 +0200

    Project Initiated | Auth With Socialite | Login With FacebookAPI

---
+++ config/services.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+    'facebook' => [
+        'client_id' => '115526855773890',
+        'client_secret' => '607d0986cc43b1f0e17be0175c2e068c',
+        'redirect' => 'http://localhost:8080/locateAndPay/public/callback',
+    ],
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+];
